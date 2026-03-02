792e9d454e9f1847d3b3c5268f38f0e04cea0bb7
Pandapple22 <willy.chung75@gmail.com>
2026-03-02 16:48:47 +0100

    Add solution for transformers-tokenization

---
+++ transformer/transformers-tokenization/transformers-tokenization.py
@@ -0,0 +1,52 @@
+import numpy as np
+from typing import List, Dict
+
+class SimpleTokenizer:
+    """
+    A word-level tokenizer with special tokens.
+    """
+    
+    def __init__(self):
+        self.word_to_id: Dict[str, int] = {}
+        self.id_to_word: Dict[int, str] = {}
+        self.vocab_size = 0
+        
+        # Special tokens
+        self.pad_token = "<PAD>"
+        self.unk_token = "<UNK>"
+        self.bos_token = "<BOS>"
+        self.eos_token = "<EOS>"
+    
+    def build_vocab(self, texts: List[str]) -> None:
+        """
+        Build vocabulary from a list of texts.
+        Add special tokens first, then unique words.
+        """        
+        for special_token in [self.pad_token, self.unk_token, self.bos_token, self.eos_token]:
+            self.word_to_id[special_token] = self.vocab_size
+            self.id_to_word[self.vocab_size] = special_token
+            self.vocab_size += 1
+            
+        for text in texts:
+            words = text.split(" ")
+            for word in words:
+                if word not in self.word_to_id:
+                    self.word_to_id[word] = self.vocab_size
+                    self.id_to_word[self.vocab_size] = word
+                    self.vocab_size += 1
+            
+    def encode(self, text: str) -> List[int]:
+        """
+        Convert text to list of token IDs.
+        Use UNK for unknown words.
+        """
+        words = text.split(" ")
+        token_ids = [self.word_to_id.get(word, self.word_to_id[self.unk_token]) for word in words]
+        return token_ids
+    
+    def decode(self, ids: List[int]) -> str:
+        """
+        Convert list of token IDs back to text.
+        """            
+        decoded_text = [self.id_to_word[id] for id in ids]
+        return " ".join(decoded_text)
