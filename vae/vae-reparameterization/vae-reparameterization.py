72c4b79e18f47c27b7a8aea71521f61bc70cbccb
Pandapple22 <willy.chung75@gmail.com>
2026-04-06 11:29:47 +0200

    Add solution for vae-reparameterization

---
+++ vae/vae-reparameterization/vae-reparameterization.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def reparameterize(mu: np.ndarray, log_var: np.ndarray) -> np.ndarray:
+    """
+    Sample from latent distribution using reparameterization trick.
+    """
+
+    std = np.exp(0.5 * log_var)
+    eps = np.random.normal(size=(mu.shape))
+    z = mu + std * eps
+    return z
